7ddcd22a7768bc1f6bdeeed1a4967b9fca360e55
Dmitry Saunin <saunindm@gmail.com>
2023-10-11 01:35:48 +0300

    src/index.js update imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './css/style.css';
 
-import './js/characters';
-
+import './js/class/Bowman';
+import './js/class/Character';
+import './js/class/Daemon';
+import './js/class/Magician';
+import './js/class/Swordsman';
+import './js/class/Undead';
+import './js/class/Zombie';
 // TODO: write your code in app.js
